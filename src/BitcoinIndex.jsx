481b0818f5c4e4ccdb9e7d21e966be1b6372d923
Berni Wong <3colouredmochi@gmail.com>
2023-01-08 23:28:09 +1100

    Fetched supported currencies

--- src/BitcoinIndex.jsx
+++ src/BitcoinIndex.jsx
@@ -5,29 +5,27 @@ const BitcoinIndex = ({ currency="AUD" }) => {
 
     // Mount and update
     useEffect(() => {
-        console.log('useEffect triggered')
+        console.log("useEffect triggered")
     })
 
-    // Mount only
+    // Mount and currency change
     useEffect(() => {
-      fetch('https://api.coindesk.com/v1/bpi/currentprice/AUD.json')
-        .then(res => res.json())
-        .then(data => setPrice(data.bpi.AUD.rate_float))
-    }, [])
+        fetch(`http://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+            .then((res) => res.json())
+            .then((data) => setPrice(data.bpi[currency].rate_float))
+    }, [currency])
 
     // Mount and price update only
     useEffect(() => {
-        console.log('useEffect on price update')
+        console.log("useEffect on price update")
     }, [price])
 
-    
-    
     return (
-    <>
-        <h1>Bitcoin Index</h1>
-        {price > 0 ? <h3>Current Price: AUD {price}</h3> : <h3>Loading...</h3>}
-    </>
-  )
+        <>
+            <h1>Bitcoin Index</h1>
+            {price > 0 ? <h3>Current Price: {currency} {price}</h3> : <h3>Loading ...</h3>}
+        </>
+    )
 }
 
 export default BitcoinIndex
--- src/CurrencySelector.jsx
+++ src/CurrencySelector.jsx
@@ -1,12 +1,23 @@
-const CurrencySelector = ({ currency }) => {
-    
-  return (
-    <select onChange={evt => setCurrency(evt.target.value)} value={currency}>
-        <option value="AUD">Australian Dollar</option>
-        <option value="USD">US Dollar</option>
-        <option value="EUR">Euro</option>
-    </select>
-  )
+import { useEffect, useState } from "react"
+
+const CurrencySelector = ({ currency, setCurrency }) => {
+    let [currencies, setCurrencies] = useState([])
+
+    useEffect(() => {
+        fetch("https://justcors.com/tl_94c41d8/http://api.coindesk.com/v1/bpi/supported-currencies.json")
+            .then((res) => res.json())
+            .then((data) => setCurrencies(data))
+    }, [])
+
+    return (
+        <select onChange={(evt) => setCurrency(evt.target.value)} value={currency}>
+            {currencies.map(cur => (
+                <option key={cur.currency} value={cur.currency}>
+                    {cur.currency} - {cur.country}
+                </option>
+            ))}
+        </select>
+    )
 }
 
 export default CurrencySelector
